2f224a9b37389e73f165ce52f15c02240e38c185
Aleksey Pekar <fenix_al@tut.by>
2023-11-13 02:54:46 +0300

    feat: add search tests

--- src/components/CardList.test.tsx
+++ src/components/CardList.test.tsx
@@ -1,68 +1,51 @@
-import { describe, expect, it } from 'vitest';
-import CardList from './CardList';
-import { PersonsContext } from '../context/PersonsContext';
-import { SearchContext } from '../context/SearchContext';
-import { render, screen } from '@testing-library/react';
-import { mockPersons } from './mockPersons';
+import { userEvent } from '@testing-library/user-event';
+import { act, render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import Pagination from './Pagination';
+import { SearchContext } from '../context/SearchContext';
+import { PersonsContext } from '../context/PersonsContext';
 
-// Test cases
-describe('CardList component', () => {
-  it('renders the specified number of cards', () => {
-    // Render the component with 10 cards per page and current page 1
-    const routes = [
-      {
-        path: '/',
-        element: (
-          <PersonsContext.Provider value={{ results: mockPersons, count: 10 }}>
-            <SearchContext.Provider value={{ searchValue: '', perPage: '10' }}>
-              <CardList currentPage={1} />
-            </SearchContext.Provider>
-          </PersonsContext.Provider>
-        ),
-      },
-    ];
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
-    });
+// use userEvent.setup outside the describe block
+userEvent.setup();
 
-    render(<RouterProvider router={router} />);
+describe('Pagination', () => {
+  const setPage = vi.fn();
 
-    const cards: HTMLElement[] = screen.getAllByRole('link');
-    expect(cards).toHaveLength(10);
-
-    // Expect to find the names of the first 5 persons on the cards
-    expect(screen.getByText('Luke Skywalker')).toHaveTextContent(
-      'Luke Skywalker'
-    );
-    expect(screen.getByText('C-3PO')).toHaveTextContent('C-3PO');
-    expect(screen.getByText('R2-D2')).toHaveTextContent('R2-D2');
-    expect(screen.getByText('Darth Vader')).toHaveTextContent('Darth Vader');
-    expect(screen.getByText('Leia Organa')).toHaveTextContent('Leia Organa');
+  const routes = [
+    {
+      path: '/',
+      element: (
+        <PersonsContext.Provider value={{ results: [], count: 20 }}>
+          <SearchContext.Provider value={{ searchValue: '', perPage: '10' }}>
+            <Pagination currentPage={1} handlePageChange={setPage} />
+          </SearchContext.Provider>
+        </PersonsContext.Provider>
+      ),
+    },
+  ];
+  const router = createMemoryRouter(routes, {
+    initialEntries: ['/'],
   });
 
-  it('renders the appropriate message if there are no cards', () => {
-    // Render the component with an empty array of persons
-    const routes = [
-      {
-        path: '/',
-        element: (
-          <PersonsContext.Provider value={{ results: [], count: 0 }}>
-            <SearchContext.Provider value={{ searchValue: '', perPage: '10' }}>
-              <CardList currentPage={1} />
-            </SearchContext.Provider>
-          </PersonsContext.Provider>
-        ),
-      },
-    ];
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
+  beforeEach(() => {
+    act(() => {
+      render(<RouterProvider router={router} />);
     });
+  });
 
-    render(<RouterProvider router={router} />);
+  describe('Make sure the component updates URL query parameter when page changes', async () => {
+    it('change param', async () => {
+      // get the button element by its role and text
+      const secondButton = screen.getByRole('button', { name: '2' });
 
-    // Expect to find the message "No results" on the screen
-    const message = screen.getByText('No results');
-    expect(message).toHaveTextContent('No results');
+      await act(async () => {
+        await userEvent.click(secondButton);
+      });
+
+      // use router.location.search to check the URL query parameter
+      expect(location.search).toBe(`?page=2`);
+    });
   });
 });
